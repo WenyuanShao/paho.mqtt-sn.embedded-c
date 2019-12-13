3dd3c0f4032ae2005a7f465479af3f60846fcc3c
WenyuanShao <shaowy@gwmail.gwu.edu>
2019-12-13 18:45:28 -0500

    refine the client

--- MQTTSNPacket/samples/makerun.sh
+++ MQTTSNPacket/samples/makerun.sh
@@ -2,4 +2,4 @@
 
 rm pub0sub1
 ./build
-./pub0sub1 --host 10.10.1.2 --clientid kkkkkkkkkk000000 --server_port 1883 --client_port 11211 --qos 1
+./pub0sub1 --host 127.0.0.1 --clientid kkkkkkkkkk000000 --server_port 1885 --client_port 11211 --qos 1 --rate 500 --pubnum 2000
--- MQTTSNPacket/samples/pub0sub1.c
+++ MQTTSNPacket/samples/pub0sub1.c
@@ -30,6 +30,60 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define CPU_FREQ 2700
+
+struct quantum {
+	unsigned long long last;
+	unsigned long long limit;
+	unsigned long long start;
+	unsigned long long size;
+};
+
+struct quantum global_limitor;
+unsigned long send_rate_limit = 0;
+int num_pub_req = 0;
+
+static inline uint64_t
+ps_tsc(void)
+{
+	unsigned long a, d, c;
+
+	__asm__ __volatile__("rdtsc" : "=a" (a), "=d" (d), "=c" (c) : : );
+
+	return ((uint64_t)d << 32) | (uint64_t)a;
+}
+
+static void
+quantum_init(struct quantum *q, unsigned long limit)
+{
+	memset(q, 0, sizeof(struct quantum));
+	q->limit = limit;
+	if (limit > 0) {
+		q->size = 1000000 / limit * CPU_FREQ;
+	}
+}
+
+static void
+quantum_start(struct quantum *q)
+{
+	q->last  = 1;
+	q->start = ps_tsc();
+}
+
+static void
+quantum_wait(struct quantum *q)
+{
+	unsigned long long cur;
+	int num;
+
+	if(q->limit == 0) return;
+	do {
+		cur = ps_tsc();
+		num = (cur - q->start) / q->size;
+	} while (q->last >= num);
+	q->last++;
+}
+
 struct opts_struct
 {
 	char* clientid;
@@ -39,7 +93,15 @@ struct opts_struct
 	int   client_port;
 };
 
+struct result
+{
+	int finished;
+	unsigned long res_dist[10];
+	unsigned long avg;
+};
+
 struct opts_struct opts = {(char*)"testClient", 1, "10.10.1.2", 443};
+struct result result;
 
 void getopts(int argc, char** argv)
 {
@@ -66,6 +128,14 @@ void getopts(int argc, char** argv)
 			if (++count < argc)
 				opts.qos = atoi(argv[count]);
 		}
+		else if (strcmp(argv[count], "--rate") == 0) {
+			if (++count < argc)
+				send_rate_limit = (unsigned long)atoi(argv[count]);
+		}
+		else if (strcmp(argv[count], "--pubnum") == 0) {
+			if (++count <argc)
+				num_pub_req = atoi(argv[count]);
+		}
 		count ++;
 	}
 }
@@ -78,6 +148,7 @@ int main(int argc, char** argv)
 	int buflen = sizeof(buf);
 	MQTTSN_topicid topic;
 	struct sockaddr_in clientaddr;
+	quantum_init(&global_limitor, send_rate_limit);
 
 	unsigned char* payload = (unsigned char*)"XXXXXXXXXXXXXXXX";
 	int payloadlen = strlen((char*)payload);
@@ -91,13 +162,16 @@ int main(int argc, char** argv)
 	//int port = 1883;
 	MQTTSNPacket_connectData options = MQTTSNPacket_connectData_initializer;
 	unsigned short topicid;
-
+	int cnt = 0;
+	unsigned long long start, end;
 	if (argc > 1)
 		getopts(argc, argv);
 
 	int port = opts.server_port;
 	char *host = opts.host;
 	int qos = opts.qos;
+	unsigned long long *res_array;
+	res_array = malloc(sizeof(unsigned long long) * num_pub_req);
 
 	mysock = transport_open();
 	if(mysock < 0)
@@ -113,7 +187,7 @@ int main(int argc, char** argv)
 		perror("bind");
 	}
 
-	printf("Sending to hostname %s port %d\n", host, port);
+	//printf("Sending to hostname %s port %d\n", host, port);
 
 	//options.clientID.cstring = "pub0sub1 MQTT-SN";
 	options.clientID.cstring = opts.clientid;
@@ -128,18 +202,18 @@ int main(int argc, char** argv)
 
 		if (MQTTSNDeserialize_connack(&connack_rc, buf, buflen) != 1 || connack_rc != 0)
 		{
-			printf("Unable to connect, return code %d\n", connack_rc);
+			//printf("Unable to connect, return code %d\n", connack_rc);
 			goto exit;
 		}
-		else 
-			printf("connected rc %d\n", connack_rc);
+		//else 
+			//printf("connected rc %d\n", connack_rc);
 	}
 	else
 		goto exit;
 
 
 	/* subscribe */
-	printf("Subscribing\n");
+	//printf("Subscribing\n");
 	topic.type = MQTTSN_TOPIC_TYPE_NORMAL;
 	topic.data.long_.name = topicname;
 	topic.data.long_.len = strlen(topic.data.long_.name);
@@ -155,67 +229,81 @@ int main(int argc, char** argv)
 		rc = MQTTSNDeserialize_suback(&granted_qos, &topicid, &submsgid, &returncode, buf, buflen);
 		if (granted_qos != 2 || returncode != 0)
 		{
-			printf("granted qos != 2, %d return code %d\n", granted_qos, returncode);
+			//printf("granted qos != 2, %d return code %d\n", granted_qos, returncode);
 			goto exit;
 		}
-		else
-			printf("suback topic id %d\n", topicid);
+		//else
+			//printf("suback topic id %d\n", topicid);
 	}
 	else
 		goto exit;
 
-	printf("Publishing\n");
+	//printf("Publishing\n");
+	quantum_start(&global_limitor);
 	/* publish with short name */
-	topic.type = MQTTSN_TOPIC_TYPE_NORMAL;
-	topic.data.id = topicid;
-	++packetid;
-	len = MQTTSNSerialize_publish(buf, buflen, dup, qos, retained, packetid,
-			topic, payload, payloadlen);
-	rc = transport_sendPacketBuffer(host, port, buf, len);
+	do {
+		quantum_wait(&global_limitor);
+		topic.type = MQTTSN_TOPIC_TYPE_NORMAL;
+		topic.data.id = topicid;
+		++packetid;
+		len = MQTTSNSerialize_publish(buf, buflen, dup, qos, retained, packetid,
+				topic, payload, payloadlen);
+		rc = transport_sendPacketBuffer(host, port, buf, len);
 
-	/* wait for puback */
-	if (MQTTSNPacket_read(buf, buflen, transport_getdata) == MQTTSN_PUBACK)
-	{
-		unsigned short packet_id, topic_id;
-		unsigned char returncode;
-
-		if (MQTTSNDeserialize_puback(&topic_id, &packet_id, &returncode, buf, buflen) != 1 || returncode != MQTTSN_RC_ACCEPTED)
-			printf("Unable to publish, return code %d\n", returncode);
-		else 
-			printf("puback received, msgid %d topic id %d\n", packet_id, topic_id);
-	}
-	else
-		goto exit;
-
-	printf("Receive publish\n");
-	if (MQTTSNPacket_read(buf, buflen, transport_getdata) == MQTTSN_PUBLISH)
-	{
-		unsigned short packet_id;
-		int qos, payloadlen;
-		unsigned char* payload;
-		unsigned char dup, retained;
-		MQTTSN_topicid pubtopic;
-
-		if (MQTTSNDeserialize_publish(&dup, &qos, &retained, &packet_id, &pubtopic,
-				&payload, &payloadlen, buf, buflen) != 1)
-			printf("Error deserializing publish\n");
-		else 
-			printf("publish received, id %d qos %d\n", packet_id, qos);
-
-		if (qos == 1)
+		/* wait for puback */
+		if (MQTTSNPacket_read(buf, buflen, transport_getdata) == MQTTSN_PUBACK)
 		{
-			len = MQTTSNSerialize_puback(buf, buflen, pubtopic.data.id, packet_id, MQTTSN_RC_ACCEPTED);
-			rc = transport_sendPacketBuffer(host, port, buf, len);
-			if (rc == 0)
-				printf("puback sent\n");
-		}
-	}
-	else
-		goto exit;
+			unsigned short packet_id, topic_id;
+			unsigned char returncode;
 
+			if (MQTTSNDeserialize_puback(&topic_id, &packet_id, &returncode, buf, buflen) != 1 || returncode != MQTTSN_RC_ACCEPTED) {
+				//printf("Unable to publish, return code %d\n", returncode);
+			} else {
+				result.finished++;
+				//printf("puback received, msgid %d topic id %d\n", packet_id, topic_id);
+			}
+		}
+		else
+			goto exit;
+
+		//printf("Receive publish\n");
+		start = ps_tsc();
+		if (MQTTSNPacket_read(buf, buflen, transport_getdata) == MQTTSN_PUBLISH)
+		{
+			unsigned short packet_id;
+			int qos, payloadlen;
+			unsigned char* payload;
+			unsigned char dup, retained;
+			MQTTSN_topicid pubtopic;
+			
+			MQTTSNDeserialize_publish(&dup, &qos, &retained, &packet_id, &pubtopic, &payload, &payloadlen, buf, buflen);
+
+			/*if (MQTTSNDeserialize_publish(&dup, &qos, &retained, &packet_id, &pubtopic,
+					&payload, &payloadlen, buf, buflen) != 1)
+				printf("Error deserializing publish\n");
+			else 
+				printf("publish received, id %d qos %d\n", packet_id, qos);
+			*/
+			if (qos == 1)
+			{
+				len = MQTTSNSerialize_puback(buf, buflen, pubtopic.data.id, packet_id, MQTTSN_RC_ACCEPTED);
+				rc = transport_sendPacketBuffer(host, port, buf, len);
+				//if (rc == 0)
+					//printf("puback sent\n");
+			}
+			end = ps_tsc();
+			res_array[cnt] = end - start;
+			cnt ++;
+		}
+		else
+			goto exit;
+	} while (cnt < num_pub_req);
+	
+	printf("publish request success: %d, Error: %d\n", cnt, num_pub_req-cnt);
 	len = MQTTSNSerialize_disconnect(buf, buflen, 0);
 	rc = transport_sendPacketBuffer(host, port, buf, len);
 
+	MQTTSNPacket_read(buf, buflen, transport_getdata);
 exit:
 	transport_close();
 
